4f72b307a97b0976237a4d04ea9c9b46853b3b41
ak3102 <87615296+ak3102@users.noreply.github.com>
2024-03-31 22:05:03 +0530

    Update firebaseAdmin.js

--- JS/firebaseAdmin.js
+++ JS/firebaseAdmin.js
@@ -1,10 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBuBNnKt824CYo1MVNoa9QoG8DRvwPVs30",
+//     authDomain: "authentication-a15de.firebaseapp.com",
+//     projectId: "authentication-a15de",
+//     storageBucket: "authentication-a15de.appspot.com",
+//     messagingSenderId: "209432881038",
+//     appId: "1:209432881038:web:bb26c5dd3fc7cc6e2126df",
+//   };
 const firebaseConfig = {
-    apiKey: "AIzaSyBuBNnKt824CYo1MVNoa9QoG8DRvwPVs30",
-    authDomain: "authentication-a15de.firebaseapp.com",
-    projectId: "authentication-a15de",
-    storageBucket: "authentication-a15de.appspot.com",
-    messagingSenderId: "209432881038",
-    appId: "1:209432881038:web:bb26c5dd3fc7cc6e2126df",
+    apiKey: "AIzaSyA2DctRduvDuy6Y7ZLSPrEjKeE6M45UKLE",
+    authDomain: "miniproject2-5b35c.firebaseapp.com",
+    projectId: "miniproject2-5b35c",
+    storageBucket: "miniproject2-5b35c.appspot.com",
+    messagingSenderId: "203872398690",
+    appId: "1:203872398690:web:cb005db9c95d16923e503b",
+    measurementId: "G-W0C4TJNNW8"
   };
 
   // Initialize Firebase
